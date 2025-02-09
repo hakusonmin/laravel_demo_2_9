4449cfa1ff2cc2d8d477bc314d3ebc2643864d12
hakusonmin <fujitakosei7@gmail.com>
2025-02-09 14:04:08 +0900

    シーディング

---
+++ app/Http/Controllers/MovieSheetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMovieSheetRequest;
+use App\Http\Requests\UpdateMovieSheetRequest;
+use App\Models\MovieSheet;
+
+class MovieSheetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMovieSheetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MovieSheet $movieSheet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MovieSheet $movieSheet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMovieSheetRequest $request, MovieSheet $movieSheet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MovieSheet $movieSheet)
+    {
+        //
+    }
+}
