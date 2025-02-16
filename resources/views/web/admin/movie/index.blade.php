4a6c7224a7fb922c9866df0d4dd1e9e46c114a34
hakusonmin <fujitakosei7@gmail.com>
2025-02-16 17:54:26 +0900

    css 更新

--- resources/views/web/admin/movie/index.blade.php
+++ resources/views/web/admin/movie/index.blade.php
@@ -8,12 +8,14 @@
     <link rel="stylesheet" href="{{ asset('./css/list.css') }}">
 </head>
 <body>
+  <h2 class="hoge">映画登録サイト</h2>
   <div class="wrapper">
+    <h2 class="wrapper-title">映画一覧</h2>
     <div class="card-container">
         @foreach ($movies as $movie)
             <div class="card">
-                <h2 class="card-title">{{ $movie->title }}</h2>
                 <img class="image" src="{{ asset('./../storage/img/' . $movie->image_url )}}">
+                <h2 class="card-title">{{ $movie->title }}</h2>
                 <button>詳細画面に行く</button>
             </div>
         @endforeach
