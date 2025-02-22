ff52f73c42bf1afa3d96caf0fa03412f07add7e2
hakusonmin <fujitakosei7@gmail.com>
2025-02-22 22:33:14 +0900

    リンター

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -10,69 +10,69 @@ use App\Models\Movie;
 
 class MovieController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index()
-  {
-    $movies = Movie::all();
-    return view('web.admin.movie.index', compact('movies'));
-  }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('web.admin.movie.index', compact('movies'));
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   */
-  public function create()
-  {
-    return view('web.admin.movie.create');
-  }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('web.admin.movie.create');
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   */
-  public function store(StoreMovieRequest $request)
-  {
-    $model = new Movie();
-    $model->title = $request->title;
-    $model->image_url = $request->image_url;
-    $model->published_date = $request->published_date;
-    $model->is_showing = $request->is_showing;
-    $model->description = $request->description;
-    $model->save();
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMovieRequest $request)
+    {
+        $model = new Movie();
+        $model->title = $request->title;
+        $model->image_url = $request->image_url;
+        $model->published_date = $request->published_date;
+        $model->is_showing = $request->is_showing;
+        $model->description = $request->description;
+        $model->save();
 
-    return redirect()->route('admin.movies.index');
-  }
+        return redirect()->route('admin.movies.index');
+    }
 
-  /**
-   * Display the specified resource.
-   */
-  public function show($id)
-  {
-    $movie = Movie::find($id);
-    return view('web.admin.movie.show', compact('movie'));
-  }
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $movie = Movie::find($id);
+        return view('web.admin.movie.show', compact('movie'));
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   */
-  public function edit(Movie $movie)
-  {
-    //
-  }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Movie $movie)
+    {
+        //
+    }
 
-  /**
-   * Update the specified resource in storage.
-   */
-  public function update(UpdateMovieRequest $request, Movie $movie)
-  {
-    //
-  }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMovieRequest $request, Movie $movie)
+    {
+        //
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   */
-  public function destroy(Movie $movie)
-  {
-    //
-  }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Movie $movie)
+    {
+        //
+    }
 }
--- resources/views/web/admin/movie_sheet/list-by-movie.blade.php
+++ resources/views/web/admin/movie_sheet/list-by-movie.blade.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movie</title>
-    <link rel="stylesheet" href="{{ asset('./css/admin/sheet/list-by-hall.css') }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Movie</title>
+  <link rel="stylesheet" href="{{ asset('./css/admin/sheet/list-by-hall.css') }}">
 </head>
+
 <body>
   <div class="wrapper">
     <h2 class="wrapper-title">チケット一覧</h2>
     <div class="card-container">
-        @foreach ($movieSheets as $movieSheet)
-            <div class="card">
-                <img class="image" src="{{ asset('./../storage/img/theater.jpg' )}}">
-                <div class="card-title">{{ $movieSheet->price }}</div>
-            </div>
-        @endforeach
+      @foreach ($movieSheets as $movieSheet)
+        <div class="card">
+          <img class="image" src="{{ asset('./../storage/img/theater.jpg') }}">
+          <div class="card-title">{{ $movieSheet->price }}</div>
+        </div>
+      @endforeach
     </div>
-    <button class="back-button" type="button" onclick="location.href='{{ route('admin.movies.show', ['movie'=> $movieSheet->movie_id]  ) }}'">戻る</button>
+    <button class="back-button" type="button" onclick="location.href='{{ route('admin.movies.show', ['movie' => $movieSheet->movie_id]) }}'">戻る</button>
   </div>
 
-    @if (session('message'))
-      <div class="alert alert-danger">
-          {{ session('message') }}
-      </div>
-    @endif
+  @if (session('message'))
+    <div class="alert alert-danger">
+      {{ session('message') }}
+    </div>
+  @endif
 </body>
+
 </html>
